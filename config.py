abafdb3bd799482ff34f0968c49f2dcae5928264
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2024-01-24 19:04:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ UPSTREAM_REPO = getenv(
    "UPSTREAM_REPO",
    "https://github.com/Prabhas505/MUSICBOT",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Prabas")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Rudra")
 GIT_TOKEN = getenv(
    "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
